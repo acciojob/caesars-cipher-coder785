26f854bc60d857f51fecd5a500a443fe942f78b3
coder785 <116183840+coder785@users.noreply.github.com>
2023-06-30 09:02:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-// Your Script here.
-// Your Script here.
-
 const lookup = {
   A: "N",
   B: "O",
@@ -32,24 +29,23 @@ const lookup = {
   ",": ",",
 };
 
-function rot13(str) {
-  let decodedArr = []; // Your Result goes here
+function rot13(encodedStr) {
+  let decodedArr =[]; // Your Result goes here
   // Only change code below this line
-	for(let i=0;i<str.length;i++){
-		if(lookup[str[i]]===undefined){
-			decodedArr.push(str[i]);
-		}
-		else{
-			decodedArr.push(lookup[str[i]]);
-		}
-		
-		
-	}
-
+   for(int i=0;i<encodedStr.length;i++){
+	   if(lookup[encodedStr[i]==undefined){
+		   decodedArr.push(encodedStr[i]);
+	   }
+	   else{
+		   decodedArr.push(lookup[encodedStr[i]]);
+	   }
+   }
   return decodedArr; //return decodedArr
 }
 
-// You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
+// You can test your code by running the above function and printing it 
+//to console by pressing the run button at the top.
+//To run it with input 36, uncomment the following line
 
 // console.log(rot13("SERR YBIR? NPPVBWBO"));
 
